1eedfbde5726ed84eeba2a856c324cfe76f7d7df
Shanku Roy <64715768+shankuroy@users.noreply.github.com>
2026-04-30 16:31:00 +1000

    scripts: lima: add rebuild script

---
+++ scripts/lima/templates/rebuild.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+NAME="${1:-ubuntu-lts}"
+CLONE="$NAME-clone"
+TEMPLATE="./$NAME.yml"
+
+GREEN='\e[32m'
+DEFAULT='\e[39m'
+
+info() {
+  echo -e "$GREEN[INFO]$DEFAULT $1"
+}
+
+TIMEFORMAT="[TIME] real %lR, user %lU, sys %lS"
+
+time {
+
+  info "rebuilding $NAME (and clone $CLONE) from template: $TEMPLATE"
+
+  info "stopping $NAME"
+  time limactl stop -f "$NAME"
+
+  info "deleting $NAME"
+  time limactl delete -f "$NAME"
+
+  info "creating $NAME from template $TEMPLATE"
+  time limactl create -y --name "$NAME" "$TEMPLATE"
+
+  info "starting $NAME"
+  time limactl start "$NAME" --mount-none
+
+  info "stopping $NAME"
+  time limactl stop -f "$NAME"
+
+  info "stopping $CLONE"
+  time limactl stop -f "$CLONE"
+
+  info "deleting $CLONE"
+  time limactl delete -f "$CLONE"
+
+  info "cloning $NAME to $CLONE"
+  time limactl clone "$NAME" "$CLONE" --start --mount-none
+
+  info "DONE!"
+
+}
+
