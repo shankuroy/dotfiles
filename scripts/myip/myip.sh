211aeb11ac50996e719a6120b2a1459c5aee35d4
Shanku Roy <64715768+shankuroy@users.noreply.github.com>
2026-05-13 23:18:36 +1000

    scripts: shorter myip

--- scripts/myip/myip.sh
+++ scripts/myip/myip.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-PUBLIC_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-LOCAL_IP="$(ipconfig getifaddr en0)"
-
-echo "public $PUBLIC_IP"
-echo " local $LOCAL_IP"
+echo " local: $(ipconfig getifaddr en0)"
+echo "public: $(dig +short myip.opendns.com @resolver1.opendns.com)"
