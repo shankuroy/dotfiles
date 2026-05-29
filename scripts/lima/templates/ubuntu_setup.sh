ccb9da728b7d53a6206d48de2b95de20c9cf4871
Shanku Roy <64715768+shankuroy@users.noreply.github.com>
2026-05-29 12:29:54 +1000

    lima: nicer logging and error handling in ubuntu_setup

--- scripts/lima/templates/ubuntu_setup.sh
+++ scripts/lima/templates/ubuntu_setup.sh
@@ -4,15 +4,29 @@
 
 set -euo pipefail # Exit on error, unset variables, or pipe failures
 
-# Ensure non-interactive apt installations
+function log_info {
+  echo "[INFO][SETUP]: $@"
+}
+
+log_info "Starting Ubuntu setup"
+
 export DEBIAN_FRONTEND=noninteractive
 
-echo "==> Preparing repositories and keys..."
+if command -v docker >/dev/null 2>&1; then
+  log_info "Docker is installed"
+  docker info
+else
+  # https://docs.docker.com/engine/install/ubuntu
+  log_info "Adding Docker's official GPG key"
+  sudo apt-get update
+  sudo apt-get install -y ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  log_info "GPG key added"
 
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+  log_info "Adding Docker repository to Apt sources"
+  sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
@@ -20,41 +34,37 @@ Components: stable
 Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
+  log_info "Docker Apt repository added"
 
-echo "==> Installing packages..."
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt-get install -y \
-    ca-certificates \
-    neovim \
-    tmux \
-    docker-ce \
-    docker-ce-cli \
-    containerd.io \
-    docker-buildx-plugin \
-    docker-compose-plugin
+  log_info "Installing Docker"
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo systemctl start docker || true
+  sudo systemctl status docker || true
+  log_info "Docker installation complete"
 
-sudo apt-get update -y
-
-echo "==> Configuring user permissions..."
-if id -nG $USER | grep -qqv "docker"; then
-    sudo usermod -aG docker $USER
+  log_info "Adding Docker user permissions"
+  getent group docker || sudo groupadd docker
+  sudo usermod -aG docker "$USER"
+  log_info "Docker user permissions added"
 fi
 
-echo "==> Setting up bashrc..."
+log_info "Setting up bashrc"
 cat <<EOF > ~/.bashrc-$USER
 # GENERATED FILE - DO NOT EDIT - EDIT INSTEAD: ~/.bashrc-session
 export TERM=xterm-256color
 export EDITOR=vim
-command -v nvim >/dev/null 2>&1 && alias vim=nvim
 alias la='ls -lah'
 touch ~/.bashrc-session && source ~/.bashrc-session
 EOF
 
-INCLUDE_USER="[[ -f ~/.bashrc-$USER ]] && source ~/.bashrc-$USER"
-grep -sqF "${INCLUDE_USER}" ~/.bashrc || echo "${INCLUDE_USER}" >> ~/.bashrc
+grep -sqF "source ~/.bashrc-$USER" ~/.bashrc \
+  || echo "source ~/.bashrc-$USER" >> ~/.bashrc
 
-echo "==> Basic server hardening..."
+log_info "bashrc setup complete"
+
+# echo "==> Basic server hardening..."
 # TODO
 
-echo "==> Bootstrap complete!"
+log_info "Ubuntu server setup complete!"
 
