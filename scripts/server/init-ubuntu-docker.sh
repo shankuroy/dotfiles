cf3e6fbad7fd0bcbd88a973517a29a1436ab7dae
Shanku Roy <64715768+shankuroy@users.noreply.github.com>
2026-05-31 11:03:55 +1000

    server: cleaner init-ubuntu-docker

--- scripts/server/init-ubuntu-docker.sh
+++ scripts/server/init-ubuntu-docker.sh
@@ -11,16 +11,43 @@ function log_info {
   printf "${GREEN}[UBUNTU-SETUP]${DEFAULT} %s\n" "$@"
 }
 
-log_info "downloading docker gpg key"
+CONF=/etc/ssh/sshd_config.d/99-hardening.conf
+log_info "saving basic ssh hardening to $CONF"
+sudo tee $CONF <<'EOF'
+PasswordAuthentication no
+PermitRootLogin no
+EOF
 
+CONF=/etc/apt/apt.conf.d/50unattended-upgrades
+log_info "saving unattended upgrades config to $CONF"
+sudo tee $CONF <<'EOF'
+Unattended-Upgrade::Allowed-Origins {
+    "${distro_id}:${distro_codename}";
+    "${distro_id}:${distro_codename}-security";
+};
+Unattended-Upgrade::Remove-Unused-Dependencies "true";
+Unattended-Upgrade::Automatic-Reboot "false";
+EOF
+
+CONF=/etc/apt/apt.conf.d/20auto-upgrades
+log_info "saving auto upgrades config to $CONF"
+sudo tee $CONF <<'EOF'
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Download-Upgradeable-Packages "1";
+APT::Periodic::Unattended-Upgrade "1";
+APT::Periodic::AutocleanInterval "7";
+EOF
+
+CONF=/etc/apt/keyrings/docker.asc
+log_info "saving docker gpg key to $CONF"
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o $CONF
+sudo chmod a+r $CONF
+sudo cat $CONF
 
-log_info "docker gpg key downloaded to /etc/apt/keyrings/docker.asc"
-log_info "adding docker apt sources"
-
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+CONF=/etc/apt/sources.list.d/docker.sources
+log_info "saving docker apt sources to $CONF"
+sudo tee $CONF <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
@@ -29,11 +56,10 @@ Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-log_info "apt sources saved to /etc/apt/sources.list.d/docker.sources"
-log_info "adding docker daemon config"
-
-sudo mkdir -p /etc/docker
-sudo tee /etc/docker/daemon.json <<EOF
+CONF=/etc/docker/daemon.json
+log_info "saving docker daemon config to $CONF"
+sudo install -m 0755 -d /etc/docker
+sudo tee /etc/docker/daemon.json <<'EOF'
 {
   "no-new-privileges": true,
   "userland-proxy": false,
@@ -45,43 +71,10 @@ sudo tee /etc/docker/daemon.json <<EOF
 }
 EOF
 
-log_info "docker daemon config saved to /etc/docker/daemon.json"
-log_info "adding basic ssh hardening"
-
-sudo tee /etc/ssh/sshd_config.d/99-hardening.conf <<EOF
-PasswordAuthentication no
-PermitRootLogin no
-EOF
-
-log_info "basic ssh hardening saved to /etc/ssh/sshd_config.d/99-hardening.conf"
-log_info "configuring unattended upgrades"
-
-sudo tee /etc/apt/apt.conf.d/50unattended-upgrades <<'EOF'
-Unattended-Upgrade::Allowed-Origins {
-    "${distro_id}:${distro_codename}";
-    "${distro_id}:${distro_codename}-security";
-};
-Unattended-Upgrade::Remove-Unused-Dependencies "true";
-Unattended-Upgrade::Automatic-Reboot "false";
-EOF
-
-log_info "unattended upgrades config saved to /etc/apt/apt.conf.d/50unattended-upgrades"
-log_info "configuring auto upgrades"
-
-sudo tee /etc/apt/apt.conf.d/20auto-upgrades <<EOF
-APT::Periodic::Update-Package-Lists "1";
-APT::Periodic::Download-Upgradeable-Packages "1";
-APT::Periodic::Unattended-Upgrade "1";
-APT::Periodic::AutocleanInterval "7";
-EOF
-
-log_info "auto upgrades config saved to /etc/apt/apt.conf.d/20auto-upgrades"
-log_info "installing docker"
-
+log_info "installing packages"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-log_info "docker installation complete (check above for any errors)"
-log_info "server init complete!"
+log_info "server init complete! (check above for any errors)"
 log_info "please restart the server with sudo reboot"
 
