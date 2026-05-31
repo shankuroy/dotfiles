6d9ab0762ec0c9e4ac60faacb881d95af9ac26c9
Shanku Roy <64715768+shankuroy@users.noreply.github.com>
2026-05-31 16:05:10 +1000

    server: better docs for init-ubu

--- scripts/server/init-ubuntu-docker.sh
+++ scripts/server/init-ubuntu-docker.sh
@@ -1,26 +1,37 @@
 #!/usr/bin/env bash
 
-# Ubuntu server init script with Docker, some very basic ssh hardening,
-# and unattended security upgrades that don't require a reboot.
+# Ubuntu server init script. Includes:
+#   - basic ssh hardening
+#   - auto unattended security upgrades that don't require a reboot
+#   - Docker
 #
-# Make sure you set up your VPS firewall!
+# Copy this script to a fresh Ubuntu server and run it, or curl-bash it:
+#   curl -fsSL https://raw.githubusercontent.com/shankuroy/dotfiles/refs/heads/main/scripts/server/init-ubuntu-docker.sh | bash
+#
+# Next steps:
+#   - configure firewall at the VPS level, e.g. IP-restricted SSH
 
+# ---------------------------------------------------- functions & variables --
 GREEN='\033[32m'
 DEFAULT='\033[0m'
 function log_info {
   printf "${GREEN}[UBUNTU-SETUP]${DEFAULT} %s\n" "$@"
 }
 
-CONF=/etc/ssh/sshd_config.d/99-hardening.conf
-log_info "saving basic ssh hardening to $CONF"
-sudo tee $CONF <<'EOF'
+# ------------------------------------------------------------ ssh hardening --
+FILE=/etc/ssh/sshd_config.d/99-hardening.conf
+log_info "saving basic ssh hardening to $FILE"
+
+sudo tee $FILE <<'EOF'
 PasswordAuthentication no
 PermitRootLogin no
 EOF
 
-CONF=/etc/apt/apt.conf.d/50unattended-upgrades
-log_info "saving unattended upgrades config to $CONF"
-sudo tee $CONF <<'EOF'
+# ------------------------------------------------------ unattended upgrades --
+FILE=/etc/apt/apt.conf.d/50unattended-upgrades
+log_info "saving unattended upgrades config to $FILE"
+
+sudo tee $FILE <<'EOF'
 Unattended-Upgrade::Allowed-Origins {
     "${distro_id}:${distro_codename}";
     "${distro_id}:${distro_codename}-security";
@@ -29,25 +40,30 @@ Unattended-Upgrade::Remove-Unused-Dependencies "true";
 Unattended-Upgrade::Automatic-Reboot "false";
 EOF
 
-CONF=/etc/apt/apt.conf.d/20auto-upgrades
-log_info "saving auto upgrades config to $CONF"
-sudo tee $CONF <<'EOF'
+# ------------------------------------------------------------- auto upgrades --
+FILE=/etc/apt/apt.conf.d/20auto-upgrades
+log_info "saving auto upgrades config to $FILE"
+
+sudo tee $FILE <<'EOF'
 APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Download-Upgradeable-Packages "1";
 APT::Periodic::Unattended-Upgrade "1";
 APT::Periodic::AutocleanInterval "7";
 EOF
 
-CONF=/etc/apt/keyrings/docker.asc
-log_info "saving docker gpg key to $CONF"
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o $CONF
-sudo chmod a+r $CONF
-sudo cat $CONF
+# -------------------------------------------------------------------- docker --
+FILE=/etc/apt/keyrings/docker.asc
+log_info "saving docker gpg key to $FILE"
 
-CONF=/etc/apt/sources.list.d/docker.sources
-log_info "saving docker apt sources to $CONF"
-sudo tee $CONF <<EOF
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o $FILE
+sudo chmod a+r $FILE
+sudo cat $FILE
+
+FILE=/etc/apt/sources.list.d/docker.sources
+log_info "saving docker apt sources to $FILE"
+
+sudo tee $FILE <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
@@ -56,8 +72,9 @@ Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-CONF=/etc/docker/daemon.json
-log_info "saving docker daemon config to $CONF"
+FILE=/etc/docker/daemon.json
+log_info "saving docker daemon config to $FILE"
+
 sudo install -m 0755 -d /etc/docker
 sudo tee /etc/docker/daemon.json <<'EOF'
 {
@@ -71,10 +88,12 @@ sudo tee /etc/docker/daemon.json <<'EOF'
 }
 EOF
 
+# ------------------------------------------------------------------ packages --
 log_info "installing packages"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# -------------------------------------------------------------------- finish --
 log_info "server init complete! (check above for any errors)"
 log_info "please restart the server with sudo reboot"
 
